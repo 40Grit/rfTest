1e533779dd87ae4a5bff33eb09d5880a9fdaba73
Garrett <fortygrit@sf.net>
2013-02-22 11:25:44 -0500

    added reference material/ schematic

---
+++ documents/LCD/LcdTest.C
@@ -0,0 +1,210 @@
+#include <p18F2520.h>
+
+/* Set up the configuration bits */
+#pragma config OSC = INTIO67
+#pragma config STVREN = OFF
+#pragma config PWRT = ON
+#pragma config BOREN = OFF
+#pragma config WDT = OFF
+#pragma config PBADEN = OFF
+#pragma config CCP2MX = PORTC
+#pragma config MCLRE = OFF
+#pragma config LVP = OFF
+
+
+
+#define BYTE unsigned char
+#define WORD unsigned int
+
+
+#define RS_CNTL    0
+#define RS_DATA    1
+#define RW_WRITE   0
+#define RW_READ    1
+
+#define LCD_E      PORTCbits.RC5
+#define LCD_RW     PORTCbits.RC4
+#define LCD_RS     PORTCbits.RC3
+
+#define LCD_DATA   PORTB
+#define LCD_TRIS   TRISB
+#define TRIS_OUT   0x00
+#define TRIS_IN    0xff
+
+
+void Pause(BYTE max);
+void Msec2(void);
+void nop(void);
+
+void LcdInit(void);
+void LcdText(BYTE row, BYTE col, char *textp);
+void LcdConst(BYTE row, BYTE col, const char *textp);
+void LcdWrite(BYTE reg, BYTE data);
+void LcdBusy(BYTE reg, BYTE data);
+
+
+/* Pause
+ * delay for a duration (in milliseconds)
+ */
+void Pause(BYTE max)
+{
+   for ( ; max != 0; max--)
+   {
+      /* the function msec2 delays for 1/2 millisecond */
+      Msec2();
+      Msec2();
+   }
+}
+
+
+/* Msec2
+ * delay for 1/2 millisecond
+ */
+void Msec2(void)
+{
+   BYTE ofst;
+
+   /* this delays for .5 msec */
+   for (ofst=0; ofst<98; ofst++)
+      nop();
+}
+
+/* nop
+ * do nothing and return
+ */
+void nop(void)
+{
+}
+
+
+/* LcdInit
+ * initialize the lcd character display
+ */
+void LcdInit(void)
+{
+   PORTC = 0;
+   DDRCbits.RC3 = 0;
+   DDRCbits.RC4 = 0;
+   DDRCbits.RC5 = 0;
+
+   LCD_DATA = 0;
+   LCD_TRIS = TRIS_IN;
+
+   Pause(25);
+   LcdWrite(RS_CNTL, 0x30);
+   Pause(5);
+   LcdWrite(RS_CNTL, 0x30);
+   Msec2();
+   LcdWrite(RS_CNTL, 0x30);
+
+   LcdBusy(RS_CNTL, 0x38);
+   LcdBusy(RS_CNTL, 0x0c);
+   LcdBusy(RS_CNTL, 0x01);
+   LcdBusy(RS_CNTL, 0x06);
+   LcdBusy(RS_CNTL, 0x02);
+}
+
+
+/* Lcdtext
+ * write a string of text to the display
+ */
+void LcdText(BYTE row, BYTE col, char *textp)
+{
+     if (row != 0)
+         row = 0x40;
+
+     LcdBusy(RS_CNTL, (BYTE)(0x80 | row | col));
+
+     for ( ; *textp!='\0'; textp++)
+        LcdBusy(RS_DATA, *textp);
+}
+
+
+/* Lcdwrite
+ * write a byte to an lcd register
+ */
+void LcdWrite(BYTE reg, BYTE data)
+{
+   LCD_RS = reg;
+   LCD_RW = RW_WRITE;
+   LCD_DATA = data;
+   LCD_TRIS = TRIS_OUT;
+   LCD_E = 1;
+   nop();
+   LCD_E = 0;
+   LCD_TRIS = TRIS_IN;
+}
+
+/* LcdBusy
+ * wait for the lcd to be idle (not busy)
+ * then write a byte to an lcd register
+ */
+void LcdBusy(BYTE reg, BYTE data)
+{
+   BYTE res;
+
+   while (1)
+   {
+      LCD_RS = RS_CNTL;
+      LCD_RW = RW_READ;
+      LCD_TRIS = TRIS_IN;
+      LCD_E = 1;
+      nop();
+      res = LCD_DATA;
+      LCD_E = 0;
+      if ((res & 0x80) == 0)
+         break;
+   }
+
+   LcdWrite(reg, data);
+}
+
+
+/* init
+ * pic general initialization
+ */
+void PicInit(void)
+{
+   // configure the internal oscillator
+   OSCCON |= 0x70;            // sets 8 mhz operation
+
+   // configure 4 a-to-d channels
+   ADCON1 = 0b00001111;       // no a-to-d inputs
+   ADCON0 = 0b00000000;       // Select channel RA0/AN0
+   ADCON2 = 0b00010010;       // 4 Tad acquisition time, Fosc/32
+   ADCON0bits.ADON = 1;       // Turn on ADC
+
+   // configure timer0 as a vanilla prescaled 16-bit timer
+   T0CONbits.TMR0ON = 1;       // turn on TMR0
+   T0CONbits.T08BIT = 0;       // configured as 16-bit
+   T0CONbits.T0CS = 0;         // clock source is Fosc/4
+   T0CONbits.PSA = 0;          // enable prescalar
+   T0CONbits.T0PS2 = 1;        // 1:128 prescalar
+   T0CONbits.T0PS1 = 1;
+   T0CONbits.T0PS0 = 0;
+   TMR0H = (BYTE)(T10MSEC >> 8);
+   TMR0L = (BYTE)(T10MSEC & 0xff);
+   INTCON2bits.TMR0IP = 0;     // Timer0 will be a low priority interrupt
+   INTCONbits.TMR0IE = 1;      // Enable interrupts
+
+   // Enable low and high priority interrupts
+   RCONbits.IPEN = 1;          // enable priority level interrupts
+   INTCONbits.PEIE = 1;        // Enable the perhiphal interrupts
+   INTCONbits.GIEL = 1;        // Let the low priority interupts begin
+   INTCONbits.GIEH = 1;
+}
+
+
+void main(void)
+{
+   /* pic initialization */
+   PicInit();
+   LcdInit();
+
+   /* main loop */
+
+   while (1)
+   {
+   }
+
+}
